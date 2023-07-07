8eb581cb78e64db81205c519728a54a8dd2f2c61
awantika1201 <awantikashinde1201@gmail.com>
2023-07-07 15:59:45 +0530

    Solved the exercism exercises

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,28 +1,72 @@
+import java.util.ArrayList;
 import java.util.List;
 
-/*
+class PythagoreanTriplet {
+    private int base;
+    private int perpendicular;
+    private int hypotenuse;
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
-
-Please remove this comment when submitting your solution.
-
-*/
-public class PythagoreanTriplet{
-
-    public static List<PythagoreanTriplet> makeTripletsList() {
-
+    public PythagoreanTriplet(int base, int perpendicular, int hypotenuse) {
+        this.base = base;
+        this.perpendicular = perpendicular;
+        this.hypotenuse = hypotenuse;
     }
 
-    public int thatSumTo(int N){
-
+    public String toString() {
+        return String.format("(%d, %d, %d)", base, perpendicular, hypotenuse);
+    }
+    public boolean equals(Object o) {
+        if ( this == o ) {
+            return true;
+        } else if ( o instanceof PythagoreanTriplet ) {
+            PythagoreanTriplet pt = (PythagoreanTriplet) o;
+            return base == pt.base && perpendicular == pt.perpendicular && hypotenuse == pt.hypotenuse;
+        } else {
+            return false;
+        }
     }
 
-    public int withFactorsLessThanOrEqualTo(int N){
-
+    public static PythagoreanTripletList makeTripletsList(){
+        return new PythagoreanTripletList();
     }
 
+    public static class PythagoreanTripletList{
+        private Integer factor = null;
+        private int sum;
+        private static final double perimeterLimit = 1 + 1 + Math.sqrt(2);
 
+        public PythagoreanTripletList withFactorsLessThanOrEqualTo(int limit)
+        {
+            factor = limit;
+            return this;
+        }
+
+        public PythagoreanTripletList thatSumTo(int sum)
+        {
+            this.sum = sum;
+            if(factor == null) {
+                factor = sum;
+            }
+            return this;
+        }
+
+        public List<PythagoreanTriplet> build()
+        {
+            List<PythagoreanTriplet> triplets = new ArrayList<PythagoreanTriplet>();
+            for(int a = 1; a <= Math.floor(sum/perimeterLimit) ; ++a)
+            {
+                int numerator = (a*a) + (int)Math.pow(sum-a,2);
+                int denominator = 2 * (sum-a);
+
+                if(numerator % denominator == 0) {
+                    int c = numerator / denominator;
+                    if (c <= factor) {
+                        int b = sum - a - c;
+                        triplets.add(new PythagoreanTriplet(a, b, c));
+                    }
+                }
+            }
+            return triplets;
+        }
+    }
 }
